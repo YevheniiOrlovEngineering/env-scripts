bfcaf1effdda1eb8bdb628812cc5fecd2b600594
Yevhenii Orlov <orlovowork@gmail.com>
2022-07-22 14:21:28 +0300

    Added docker installation script

---
+++ install/docker-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#export VERSION=5:19.03.11~3-0~ubuntu-focal
+
+sudo apt update
+sudo apt remove -y docker docker-engine docker.io containerd runc docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo apt autoremove -y
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+
+sudo docker run hello-world
