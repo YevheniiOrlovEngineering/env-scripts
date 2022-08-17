a7b8cf6ed3a6723d704b3d2d2f02cae6795b53b4
Yevhenii Orlov <orlovowork@gmail.com>
2022-08-17 13:12:41 +0300

    Refactored code

--- install/bashrc-update.sh
+++ install/bashrc-update.sh
@@ -2,44 +2,44 @@
 
 CONFIG_FILE=${CONFIG_FILE:-${HOME}/.bashrc}
 
-echo >> $CONFIG_FILE
-echo "# kubectl autocompletion" >> $CONFIG_FILE
-echo "source <(kubectl completion bash)" >> $CONFIG_FILE
-echo "alias k=kubectl" >> $CONFIG_FILE
-echo "complete -F __start_kubectl k" >> $CONFIG_FILE
-echo "export KUBE_EDITOR='nano'" >> $CONFIG_FILE
-echo >> $CONFIG_FILE
+echo >> ${CONFIG_FILE}
+echo "# kubectl autocompletion" >> ${CONFIG_FILE}
+echo "source <(kubectl completion bash)" >> ${CONFIG_FILE}
+echo "alias k=kubectl" >> ${CONFIG_FILE}
+echo "complete -F __start_kubectl k" >> ${CONFIG_FILE}
+echo "export KUBE_EDITOR='nano'" >> ${CONFIG_FILE}
+echo >> ${CONFIG_FILE}
 
-echo "# Show a current active git branch in the shell prompt" >> $CONFIG_FILE
-echo "# add \t to below command to display current time" >> $CONFIG_FILE
-echo "export PS1='\[\033[01;34m\]\w\[\033[01;33m\]\$(__git_ps1)\[\033[01;34m\]\n\[\033[01;32m\]\u\[\033[01;34m\] \$\[\033[00m\] '" >> $CONFIG_FILE
-echo >> $CONFIG_FILE
+echo "# Show a current active git branch in the shell prompt" >> ${CONFIG_FILE}
+echo "# add \t to below command to display current time" >> ${CONFIG_FILE}
+echo "export PS1='\[\033[01;34m\]\w\[\033[01;33m\]\$(__git_ps1)\[\033[01;34m\]\n\[\033[01;32m\]\u\[\033[01;34m\] \$\[\033[00m\] '" >> ${CONFIG_FILE}
+echo >> ${CONFIG_FILE}
 
-echo "# Shortcut for the pretty git log. Can be extended with the commit count parameter (last -10, last -35)" >> $CONFIG_FILE
-echo "alias last='git log --graph --all --oneline --decorate '" >> $CONFIG_FILE
-echo "alias gcp='git cherry-pick'" >> $CONFIG_FILE
-echo "alias gits='git status'" >> $CONFIG_FILE
-echo "alias gitc='git checkout'" >> $CONFIG_FILE
-echo >> $CONFIG_FILE
+echo "# Shortcut for the pretty git log. Can be extended with the commit count parameter (last -10, last -35)" >> ${CONFIG_FILE}
+echo "alias last='git log --graph --all --oneline --decorate '" >> ${CONFIG_FILE}
+echo "alias gcp='git cherry-pick'" >> ${CONFIG_FILE}
+echo "alias gits='git status'" >> ${CONFIG_FILE}
+echo "alias gitc='git checkout'" >> ${CONFIG_FILE}
+echo >> ${CONFIG_FILE}
 
-echo "export KUBECONFIG=\${HOME}/.kube/config" >> $CONFIG_FILE
+echo "export KUBECONFIG=\${HOME}/.kube/config" >> ${CONFIG_FILE}
 echo "export SCRIPTS=$(pwd)/.." >> ~/.bashrc
-echo >> $CONFIG_FILE
+echo >> ${CONFIG_FILE}
 
-echo "alias ..='cd ..'" >> $CONFIG_FILE
-echo "alias ...='cd ../../'" >> $CONFIG_FILE
-echo "alias ....='cd ../../../'" >> $CONFIG_FILE
-echo "alias .....='cd ../../../../'" >> $CONFIG_FILE
-echo >> $CONFIG_FILE
+echo "alias ..='cd ..'" >> ${CONFIG_FILE}
+echo "alias ...='cd ../../'" >> ${CONFIG_FILE}
+echo "alias ....='cd ../../../'" >> ${CONFIG_FILE}
+echo "alias .....='cd ../../../../'" >> ${CONFIG_FILE}
+echo >> ${CONFIG_FILE}
 
-echo "alias kpo='k get pods -A'" >> $CONFIG_FILE
-echo "alias kdesc='k describe pods -n '" >> $CONFIG_FILE
-echo "alias klog='k logs -n '" >> $CONFIG_FILE
-echo "alias ktok='sudo kubeadm token create $(sudo kubeadm token generate) --print-join-command'" >> $CONFIG_FILE
-echo "alias kreset='\${SCRIPTS}/deploy/kube-master.sh'" >> $CONFIG_FILE
-echo >> $CONFIG_FILE
+echo "alias kpo='k get pods -A'" >> ${CONFIG_FILE}
+echo "alias kdesc='k describe pods -n '" >> ${CONFIG_FILE}
+echo "alias klog='k logs -n '" >> ${CONFIG_FILE}
+echo "alias ktok='sudo kubeadm token create $(sudo kubeadm token generate) --print-join-command'" >> ${CONFIG_FILE}
+echo "alias kreset='\${SCRIPTS}/deploy/kube-master.sh'" >> ${CONFIG_FILE}
+echo >> ${CONFIG_FILE}
 
-echo "# test internet speed connection" >> $CONFIG_FILE
-echo "alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -'" >> $CONFIG_FILE
+echo "# test internet speed connection" >> ${CONFIG_FILE}
+echo "alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -'" >> ${CONFIG_FILE}
 
-source $CONFIG_FILE
+source ${CONFIG_FILE}
--- install/go-install.sh
+++ install/go-install.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+# Open a new bash session, once script finishes, to enable env changes
+
+CONFIG_FILE=${CONFIG_FILE:-${HOME}/.bashrc}
+
 GO_VERSION=${GO_VERSION:-1.18.4}
 GO_ARCH=${GO_ARCH:-go${GO_VERSION}.linux-amd64.tar.gz}
+GO_ROOT=${GO_ROOT:-/usr/local/go}
 
-
-sudo rm -rf /usr/local/go ${HOME}/go
-sed -i '/GOPATH\|GOBIN/d' ~/.bashrc
+sudo rm -rf ${GO_ROOT} ${HOME}/go
+sed -i '/GOPATH\|GOBIN/d' ${CONFIG_FILE}
 
 wget --show-progress --continue https://golang.org/dl/${GO_ARCH}
 sudo tar -C /usr/local -xzvf ${GO_ARCH}
 rm ${GO_ARCH}
 
 mkdir -p ~/go/{bin,pkg,src}
-echo "export GOPATH=\${HOME}/go" >> ~/.bashrc
-echo "export GOBIN=\${GOPATH}/bin" >> ~/.bashrc 
-echo "export PATH=\${PATH}:/usr/local/go/bin:\${GOBIN}" >> ~/.bashrc
-sed -i -e '$a\' ~/.bashrc
 
-go version
+sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' ${CONFIG_FILE} 
+echo >> ${CONFIG_FILE}
+echo "export GOPATH=\${HOME}/go" >> ${CONFIG_FILE}
+echo "export GOBIN=\${GOPATH}/bin" >> ${CONFIG_FILE}
+echo "export PATH=\${PATH}:${GO_ROOT}/bin:\${GOBIN}" >> ${CONFIG_FILE}
+sed -i -e '$a\' ${CONFIG_FILE}
+
+/usr/local/go/bin/go version
