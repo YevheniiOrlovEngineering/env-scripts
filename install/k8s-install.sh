d833d6e46dca0837ece694f7177caad27fa30267
Yevhenii Orlov <orlovowork@gmail.com>
2023-05-18 16:44:00 +0300

    fix: Optimized k8s installation for different ubuntu distros

--- install/k8s-install.sh
+++ install/k8s-install.sh
@@ -4,22 +4,21 @@ cd "$(dirname "$0")" || exit
 
 K8S_VERSION=${K8S_VERSION:-1.22.12-00}
 
-sudo apt update
+sudo apt-get update
 
 sudo iptables -F && sudo iptables -t nat -F && sudo iptables -t mangle -F && sudo iptables -X
 sudo kubeadm reset
 sudo apt-mark unhold kubectl kubeadm kubelet kubernetes-cni
-sudo apt purge -y kubeadm kubectl kubelet kubernetes-cni
-sudo apt autoremove -y
+sudo apt-get purge -y kubeadm kubectl kubelet kubernetes-cni
+sudo apt-get autoremove -y
 sudo rm -rf /etc/cni /etc/kubernetes /var/lib/dockershim /var/lib/etcd /var/lib/kubelet /var/run/kubernetes ~/.kube/cache ~/.kube/config /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt install -y apt-transport-https ca-certificates curl
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
-  | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt install -y kubernetes-cni kubelet=${K8S_VERSION} kubeadm=${K8S_VERSION} kubectl=${K8S_VERSION}
+sudo apt-get install -y kubernetes-cni kubelet=${K8S_VERSION} kubeadm=${K8S_VERSION} kubectl=${K8S_VERSION}
 
 sudo apt-mark hold kubelet kubeadm kubectl kubernetes-cni
 
